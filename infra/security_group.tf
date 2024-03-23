7b527a49b4f414911cd2cac40c56c268350b1ed3
Fahd-DevOps <fahdkhaled777@gmail.com>
2024-03-23 08:44:05 -0700

    add ecs sg

---
+++ infra/security_group.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.test_vpc.id
+
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow-app-ssh"
+  }
+}
