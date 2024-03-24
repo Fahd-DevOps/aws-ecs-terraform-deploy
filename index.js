4c63e8d052256b216df5a1725925ab2780b149ec
Fahd-DevOps <fahdkhaled777@gmail.com>
2024-03-23 19:10:03 -0700

    test with alb

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node from ecs\n'
+  const msg = 'Hello Node from ecs with alb\n'
   res.end(msg);
 });
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
+
