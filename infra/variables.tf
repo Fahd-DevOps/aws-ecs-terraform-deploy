504e548dff3caaef7922c7246bac68dfc887aa5c
Fahd-DevOps <fahdkhaled777@gmail.com>
2024-03-23 19:03:10 -0700

    update v1.3

--- infra/variables.tf
+++ infra/variables.tf
@@ -10,6 +10,10 @@ variable "pub_sub_1" {
   default = "10.0.1.0/24"
 }
 
+variable "pub_sub_2" {
+  default = "10.0.2.0/24"
+}
+
 variable "ecs_cluster_name" {
   default = "test-ecs-cluster"
 }
@@ -20,4 +24,16 @@ variable "ecs_service_name" {
 
 variable "ecs_task_name" {
   default = "test-ecs-task"
+}
+
+variable "alb_name" {
+  default = "alb-ecs"
+}
+
+variable "lb_target_group" {
+  default = "alb-tg-ecs"
+}
+
+variable "alb_sg_name" {
+  default = "alb-security-group"
 }
