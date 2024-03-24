97b4b28d629811a1a95b684a129eaef31286b988
Fahd-DevOps <fahdkhaled777@gmail.com>
2024-03-23 19:04:09 -0700

    update sub2

--- infra/route_table_association.tf
+++ infra/route_table_association.tf
@@ -2,3 +2,8 @@ resource "aws_route_table_association" "az1-sub-plc" {
   subnet_id      = aws_subnet.test_subnet.id
   route_table_id = aws_route_table.public-rt.id
 }
+
+resource "aws_route_table_association" "az2-sub-plc" {
+  subnet_id      = aws_subnet.test_subnet_2.id
+  route_table_id = aws_route_table.public-rt.id
+}
