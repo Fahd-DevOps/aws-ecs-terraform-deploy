47e8302ef246a980b15329b143943dc2615bfb32
Fahd-DevOps <fahdkhaled777@gmail.com>
2024-03-23 19:04:30 -0700

    update alb sg

--- infra/security_group.tf
+++ infra/security_group.tf
@@ -27,3 +27,23 @@ resource "aws_security_group" "ecs_sg" {
     Name = "allow-app-ssh"
   }
 }
+
+resource "aws_security_group" "alb_sg" {
+  vpc_id      = aws_vpc.test_vpc.id
+  name        = var.alb_sg_name
+  description = "Security group for ALB and ECS service"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
